0b89ab22dd9a3f3545f57dd77af871fc1c3e879f
harrycampaz <harrycampaz@gmail.com>
2020-02-11 21:27:17 -0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,9 @@ public class Main {
 
         Facade facade = new Facade();
         System.out.println("Coke: "+ facade.dispenseCoke());
+        System.out.println("Pepsi: "+ facade.dispensePepsi());
+        System.out.println("Fanta: " + facade.dispenseFanta());
+        System.out.println("Limonada: " + facade.dispenseLimonada());
 
 
     }
